17a30e210b1a133fcda4034f0765e8c26ab8a8b1
Trent Shoun <77755160+TrentShoun@users.noreply.github.com>
2021-08-10 09:47:41 -0700

    Add files via upload
    Script to automate a connection to Azure Stack Hub storing credentials in securestring text file.

---
+++ connect-azurermaccount.ps1
@@ -0,0 +1,17 @@
+﻿#Run Once - create secure password file
+read-host -assecurestring | convertfrom-securestring | out-file c:\temp\cred.txt 
+
+
+Add-AzureRMEnvironment -Name "AzureStack" -ArmEndpoint https://management.local.azurestack.external
+$AuthEndpoint = (Get-AzureRMEnvironment -Name "AzureStack").ActiveDirectoryAuthority.TrimEnd('/')
+$AADTenantName = "trentsh.onmicrosoft.com"
+$TenantId = (invoke-restmethod "$($AuthEndpoint)/$($AADTenantName)/.well-known/openid-configuration").issuer.TrimEnd('/').Split('/')[-1]
+
+$userId = "user@yourtenant.onmicrosoft.com"
+$password = get-content -Path "c:\temp\cred.txt" | ConvertTo-SecureString
+
+#Set the powershell credential object
+$cred = New-Object -TypeName System.Management.Automation.PSCredential($userId ,$password)
+
+#log On To Azure Account
+Connect-AzureRmAccount -EnvironmentName "AzureStack" -TenantId $TenantId -Credential $cred 
